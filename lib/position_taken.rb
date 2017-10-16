db590aab621b5c8fca1fe28b124778b5487112a0
Ssenkowski <scsenkowski@gmail.com>
2017-10-16 17:06:03 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken(board, index)
-  if board[index_number] == `" "`
-  elsif board[index_number] == `""`
+  if board[index_number] != `" "`
+  elsif board[index_number] != `""`
   else nil
   end
 end
